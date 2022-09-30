3d542cea0d2dc545aa52b38d37ac6f386ae26cb5
jeba1 <jebasafa@gmail.com>
2022-09-29 21:39:53 -0500

    l

---
+++ data.sh
@@ -0,0 +1,10 @@
+#!/bin/bash          
+sudo apt-get update
+sudo apt-get install openjdk-11-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins -y
+sudo systemctl start jenkins 
+sudo systemctl status jenkins
+#sudo update-alternatives --config java
